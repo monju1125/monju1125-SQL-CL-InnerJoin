f12f16d4ca5d06fea27c57e60121a999ef72e879
monju1125 <monju1125@github.com>
2026-06-07 05:38:25 +0000

    Test run: 0/1 passed, 1 failed, 0 skipped - 2026-06-07T05:38:25.650Z

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,1 @@
-SELECT student.teacher_name, student.id, student.student_name FROM class INNER JOIN student ON class.class_title = student.class_title,  student.teacher_name = 'Ms. Lovelace';
+SELECT class.id, class.teacher_name, class.class_title FROM class INNER JOIN student ON class.class_title = student.class_title WHERE cla.teacher_name = 'Ms. Lovelace';
